25d7423570f8b713553911660ed5560e9699be07
Parjanay Sharma <sharma.parjanay@gmail.com>
2023-08-13 22:22:55 +0530

    End of Section 4 without code refactoring : javascript issue

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,40 @@
+class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id]) #Convert into a instance variable
+  end
+
+  def index
+    @articles = Article.all #Using instance variable because required in view
+  end
+  def new
+    @article = Article.new
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was deleted successfully."
+    redirect_to articles_path
+  end
+end
